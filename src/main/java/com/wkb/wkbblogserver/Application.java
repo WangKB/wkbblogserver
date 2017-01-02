569453ec43357b4ef01a77fa6b91ea72dfc6cbbb
WangKb <799870386@qq.com>
2017-01-02 22:20:07 +0800

    springboot 应用类创建+折腾idea

---
+++ src/main/java/com/wkb/wkbblogserver/Application.java
@@ -0,0 +1,15 @@
+package com.wkb.wkbblogserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by 79987 on 2017/1/2.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
