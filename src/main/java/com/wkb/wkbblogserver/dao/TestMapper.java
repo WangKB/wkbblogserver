d9bf8fccc9fb97123ce4e10553ad8f04c528b77d
WangKb <799870386@qq.com>
2017-01-10 22:00:20 +0800

    集成mapper

---
+++ src/main/java/com/wkb/wkbblogserver/dao/TestMapper.java
@@ -0,0 +1,10 @@
+package com.wkb.wkbblogserver.dao;
+
+import com.wkb.wkbblogserver.entity.TestEntity;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * Created by 79987 on 2017/1/10.
+ */
+public interface TestMapper  extends Mapper<TestEntity> {
+}
